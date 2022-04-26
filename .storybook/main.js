abbfc473e894a77e6c33015642a4a1291960da64
tuanna2704 <tuanna2704@gmail.com>
2022-04-26 22:20:27 +0700

    [Fix] Story book

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,14 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.story.mdx",
+    "../src/**/*.story.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    'addon-redux'
   ],
   "framework": "@storybook/react",
   "core": {
--- src/stories/ListItem.story.tsx
+++ src/stories/ListItem.story.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import { data } from './data';
+import { createStore } from 'redux';
+import rootReducer from '../rootReducer';
+import { enhancer } from 'addon-redux';
 import ListItem from '../components/ListItems';
+
+const store = createStore(
+  rootReducer,
+  { data: {
+    items: [
+      {id: 1, description: "Something", watchers_count: 10, language: ['JS'], open_issues: 12, privates: true},
+      {id: 2, description: "Something", watchers_count: 10, language: ['JS'], open_issues: 12, privates: true}
+    ]
+  }},
+  enhancer
+);
 
 export default {
   title: 'ListItem',
@@ -11,18 +24,14 @@ export default {
 } as ComponentMeta<typeof ListItem>;
 
 const Template: ComponentStory<typeof ListItem> = (args) => {
-  const initialState = {};
-  const mockStore = configureStore();
-  const store = mockStore(initialState);
-
   return (
-    <Provider store={store}>
-      <ListItem {...args} />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ListItem {...args} />
+      </Provider>
+    </BrowserRouter>
   );
 };
 
 export const DefaultTable = Template.bind({});
-DefaultTable.args = {
-  data,
-};
+DefaultTable.args = {};
